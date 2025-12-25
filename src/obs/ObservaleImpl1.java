a48733991a058c2afaa0f02c6479035e792ab5a2
ikrame <ikrameazaz@gmail.com>
2025-12-26 00:54:26 +0100

    Observable impl

---
+++ src/obs/ObservaleImpl1.java
@@ -0,0 +1,36 @@
+package obs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ObservaleImpl1 implements Observale {
+    private List<Observer> observers=new ArrayList<>();
+
+    private int state;
+    @Override
+    public void subscribe(Observer o) {
+        observers.add(o);
+    }
+
+    @Override
+    public void unsubsribe(Observer o) {
+        observers.remove(o);
+    }
+
+    @Override
+    public void notifyObservers() {
+        for (Observer o:observers){
+            o.update(this);
+        }
+    }
+
+
+    public int getState() {
+        return state;
+    }
+
+    public void setState(int state) {
+        this.state = state;
+        notifyObservers();
+    }
+}
